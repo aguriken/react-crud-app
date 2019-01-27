6eae245f421a1c3cbde64ba9d81984bca15b1655
中原賢一 <ohagurokenichi@yahoo.co.jp>
2019-01-27 16:12:31 +0900

    explanation simple JSX

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <div><h1 className="App-title">Hello, world!</h1></div>;
+    return <h1>Hello, world!</h1>;
   }
 }
 
